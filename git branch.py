52658eedbfe4282c9715acd69249f86ad2492124
kamalnadh321 <138582649+kamalnadh321@users.noreply.github.com>
2023-08-03 13:00:40 +0530

    Update git branch.py

---
+++ git branch.py
@@ -0,0 +1,50 @@
+import json
+import glob
+import uuid
+import os
+import pandas as pd
+#folder paths are hard coded
+#schemas.json path is also hard coded
+#modularization with reusability
+def get_columns(ds):
+    schemas_file_path = os.environ.setdefault('SCHEMAS_FILE_PATH','data/retail_db/schemas.json')
+    with open(schemas_file_path) as fp:
+        schemas = json.load(fp)
+    try:
+        schema = schemas.get(ds)
+        if not schema:
+            raise KeyError
+        cols = sorted(schema, key=lambda s: s['column_position'])
+        columns = [col['column_name'] for col in cols]
+        return columns
+    except KeyError:
+        print(f'schema not found {ds}')
+        return
+    
+def process_files(src_base_dir, ds, tgt_base_dir):
+    for file in glob.glob(f'{src_base_dir}/part*'):
+        df = pd.read_csv(file, names=get_columns(ds))
+        os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
+        df.to_json(
+                    f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
+                    orient='records',
+                    lines=True
+                )
+        print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+
+def main():
+    src_base_dir = os.environ['SRC_BASE_DIR']
+    tgt_base_dir = os.environ['TGT_BASE_DIR']
+    datasets = os.environ.get('DATASETS')
+    if not datasets:
+        for path in glob.glob(f'{src_base_dir}/*'):
+            if os.path.isdir(path):
+                process_files(src_base_dir, os.path.split(path)[1], tgt_base_dir)
+    else:
+        dirs = datasets.split(',')
+        for dis in dirs:
+            process_files(src_base_dir, ds , tgt_base_dir )
+
+            
+if __name__ == '__main__':
+    main()
